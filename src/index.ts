6c488f0fbb67f455fcaccfbe61559f64fc97783c
DevAngelSanchez <dev.angelrsanchez@gmail.com>
2025-11-24 12:17:19 -0400

    Trabajando con los problemas de cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const UPLOADS_STATIC_PATH = path.join(__dirname, '..', 'public', 'uploads');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://rifas-46tm8vmwx-devangelsanchezs-projects.vercel.app/", // Asegúrate de que esta URL sea la de tu frontend
+  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://rifas-46tm8vmwx-devangelsanchezs-projects.vercel.app/" || "https://rifas-lyart.vercel.app/", // Asegúrate de que esta URL sea la de tu frontend
   methods: "GET, HEAD, POST, PUT, PATCH, DELETE",
   credentials: true
 }));
