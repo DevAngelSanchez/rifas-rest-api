bc55deb9f6dd2e61a5195677d79b35d41bd59a89
DevAngelSanchez <dev.angelrsanchez@gmail.com>
2025-11-25 13:36:50 -0400

    reset de base de datos

--- prisma/migrations/20251114140624_ajustes_user/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "schoolId" DROP NOT NULL;
--- prisma/migrations/20251121012246_newfields_on_invoice/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Invoice" ADD COLUMN     "amountBss" DECIMAL(65,30),
-ADD COLUMN     "amountUsd" DECIMAL(65,30),
-ADD COLUMN     "bcvRate" DECIMAL(65,30),
-ADD COLUMN     "proofUrl" TEXT,
-ALTER COLUMN "reference" DROP NOT NULL;
